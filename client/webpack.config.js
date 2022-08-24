51178f5b4bed17752163956eb73eeb3c9e170916
Daniel Pryor <75132443+daniel-pryor@users.noreply.github.com>
2022-08-25 10:37:57 +1200

    Initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'index.js'),
+  output: {
+    path: path.join(__dirname, '../server/public'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../server/public'),
+  },
+}
