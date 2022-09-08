e8b12802387639669bfdf8f751c22f0ce9ab291a
daniel-pryor <danpryor13@gmail.com>
2022-09-08 18:26:50 +1200

    redux added for difficulty

--- client/reducers/difficulty.js
+++ client/reducers/difficulty.js
@@ -1,6 +1,6 @@
 import { GET_DIFFICULTY, UPDATE_DIFFICULTY } from '../actions'
 
-const initialDifficulty = ['medium']
+const initialDifficulty = 'medium'
 
 function difficulty(state = initialDifficulty, action) {
   const { type, payload } = action
@@ -9,6 +9,8 @@ function difficulty(state = initialDifficulty, action) {
       return state
     case UPDATE_DIFFICULTY:
       return payload
+    default:
+      return state
   }
 }
 
