b20b6b2be673bd8d6e5ac87247708fb4f7172784
daniel-pryor <danpryor13@gmail.com>
2022-09-13 17:26:45 +1200

    add actions test

---
+++ server/db/user.test.js
@@ -0,0 +1,19 @@
+// const knex = require('knex')
+// const config = require('./knexfile').test
+// const testDb = knex(config)
+
+// const { getUser, createUser } = require('./users')
+
+// beforeAll(() => {
+//   return testDb.migrate.latest()
+// })
+
+// beforeEach(() => {
+//   return testDb.seed.run()
+// })
+
+// describe('getUser', () => {
+//   it('return the correct user', () => {
+//     return db.getUser
+//   })
+// })
---
+++ server/routes/users.test.js
@@ -0,0 +1,8 @@
+// const request = require('supertest')
+// const { screen } = require('@testing-library/dom')
+// const server = require('./server')
+// const db = require('./db')
+
+// require('@testing-library/jest-dom')
+
+// jest.mock('./db')
